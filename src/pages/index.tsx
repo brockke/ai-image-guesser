1d7aba8a17ef312feabc1b3744c743d833e2888c
Brock Kennedy <brock97k@gmail.com>
2023-01-20 10:46:02 -0400

    Changed button styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -83,25 +83,27 @@ const Home: NextPage = () => {
             <div className="pt-2">
               <div className="grid grid-cols-3 gap-1">
                 {imageUrls.map((url) => (
-                  <img key={url.id} src={url.url} className="col-span-1" alt="Grid Image" />
+                  <button key={url.id}>
+                    <img key={url.id} src={url.url} className="col-span-1" alt="Grid Image" />
+                  </button>
                 ))}
               </div>
             </div>
           </div>
           <div className="p-2 outline outline-1 outline-gray-300">
             <div className="flex flex-1 outline-4">
-              <button className="group hover:bg-gray-200">
-                <svg className="h-8 w-8 m-2 fill-current text-gray-500 group-hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <button className="group hover:bg-gray-200 active:bg-gray-300">
+                <svg className="h-8 w-8 m-2 fill-current text-gray-500 group-hover:text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path d="M17.65 6.35A7.958 7.958 0 0 0 12 4a8 8 0 0 0-8 8 8 8 0 0 0 8 8c3.73 0 6.84-2.55 7.73-6h-2.08A5.99 5.99 0 0 1 12 18a6 6 0 0 1-6-6 6 6 0 0 1 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35Z"/>
                 </svg>
               </button>
-              <button className="group hover:bg-gray-200">
-                <svg className="h-8 w-8 m-2 fill-current text-gray-500 group-hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <button className="group hover:bg-gray-200 active:bg-gray-300">
+                <svg className="h-8 w-8 m-2 fill-current text-gray-500 group-hover:text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path d="M9 21H5q-.825 0-1.413-.587Q3 19.825 3 19v-7q0-1.875.712-3.513q.713-1.637 1.926-2.85q1.212-1.212 2.85-1.925Q10.125 3 12 3t3.513.712q1.637.713 2.85 1.925q1.212 1.213 1.925 2.85Q21 10.125 21 12v7q0 .825-.587 1.413Q19.825 21 19 21h-4v-8h4v-1q0-2.925-2.038-4.963Q14.925 5 12 5T7.038 7.037Q5 9.075 5 12v1h4Z"/>
                 </svg>
               </button>
-              <button className="group hover:bg-gray-200">
-                <svg className="h-8 w-8 m-2 fill-current text-gray-500 group-hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <button className="group hover:bg-gray-200 active:bg-gray-300">
+                <svg className="h-8 w-8 m-2 fill-current text-gray-500 group-hover:text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10zm0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16zM11 7h2v2h-2V7zm0 4h2v6h-2v-6z"/>
                 </svg>
               </button>
